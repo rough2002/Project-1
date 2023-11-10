6253960325f5bcccb9afb1f97d7847d743b4c0e1
Manav Malhotra <roughsheet.2002@gmail.com>
2023-11-10 23:09:08 +0530

    added database connection

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const dbString = process.env.MONGODB_URI.replace(
+  "<password>",
+  process.env.PASSWORD
+);
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${dbString}/${DB_NAME}`);
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
